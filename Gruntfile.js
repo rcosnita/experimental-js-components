b3f7146ece04660efa4a06c40bd866fb99f42400
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2015-06-09 17:38:59 +0300

    I added requirejs optimization step into grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 var grunt = require("grunt");
 
 grunt.loadNpmTasks('grunt-jsdoc');
+grunt.loadNpmTasks('grunt-contrib-requirejs');
 
 grunt.initConfig({
     "jsdoc" : {
@@ -10,6 +11,29 @@ grunt.initConfig({
                 "destination": "doc",
                 "configure": "jsdoc.json",
                 "private": false
+            }
+        }
+    },
+    "requirejs": {
+        "compile": {
+            "options": {
+                "baseUrl": ".",
+                "appDir": ".",
+                "dir": "dist",
+                "fileExclusionRegExp": /(node_modules|doc|doc-lib)$/,
+                "paths": {
+                    "jquery": "lib/jquery-2.1.4",
+                    "jquery-ui": "lib/jquery-ui-1.10.4.min",
+                    "text": "lib/text-2.0.14",
+                    "eventemitter": "lib/microevent",
+                    "handlebars-lib": "lib/handlebars-3.0.3",
+                    "handlebars": "src/utils/handlebars",
+                    "bootstrap": "lib/bootstrap-3.3.4/js/bootstrap",
+                    "models": "src/models",
+                    "components": "src/components",
+                    "factories": "src/factories",
+                    "utils": "src/utils"
+                }
             }
         }
     }
